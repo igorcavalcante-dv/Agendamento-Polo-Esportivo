bc291dd1b6431e8a113b87322b0a644f50804394
Igor Cavalcante <igorcavalcante2003@gmail.com>
2026-06-08 23:29:40 -0300

    Corrigindo bugs e testes com o banco

--- src/main/java/com/polodesportivo/agendamento/Configuracao/SecurityConfig.java
+++ src/main/java/com/polodesportivo/agendamento/Configuracao/SecurityConfig.java
@@ -2,23 +2,24 @@ package com.polodesportivo.agendamento.Configuracao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) throws Exception {
+
         return http
-                .csrf(csrf -> csrf.disable()) // Desativa CSRF para o Postman funcionar
-                .authorizeExchange(exchanges -> exchanges
-                        .anyExchange().permitAll() // Libera todas as rotas (sem login)
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
                 )
-                .httpBasic(httpBasic -> httpBasic.disable()) // Desativa login básico
-                .formLogin(formLogin -> formLogin.disable()) // Desativa formulário de login
+                .formLogin(form -> form.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
                 .build();
     }
 }
